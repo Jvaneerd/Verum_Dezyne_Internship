5f5553467cdcf34f474b939a1424942e001184ff
Jvaneerd <J.vaneerd@student.fontys.nl>
2017-05-09 11:15:06 +0200

    ADD LED integrated snapshot

---
+++ Tutorial_Snapshots/Alarm_System_LED_Integrated/src/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <signal.h>
+
+#include <dzn/runtime.hh>
+#include <dzn/locator.hh>
+#include <dzn/pump.hh>
+#include "AlarmSystem.hh"
+
+int
+main(int argc, char* argv[])
+{
+  dzn::locator loc;
+  dzn::runtime rt;
+
+  loc.set(rt);
+
+  AlarmSystem as(loc);
+  as.dzn_meta.name = "AlarmSystem";
+
+  as.check_bindings();
+
+  std::string input;
+  while(std::cin >> input) {
+    if(input.compare("s") == 0) {
+      as.iController.in.sensorTriggered();
+    }
+    else if(input.compare("v") == 0) {
+      as.iController.in.validPincode();
+    }
+  }
+}
