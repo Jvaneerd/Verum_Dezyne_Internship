5e735eadb02a1c911bf16fc759b84011ba5464ad
Jvaneerd <J.vaneerd@student.fontys.nl>
2017-05-12 14:25:51 +0200

    UPD Full Alarm System implementation

--- Alarm_System_Full/src/LED.hh
+++ Alarm_System_Full/src/LED.hh
@@ -8,12 +8,13 @@ class LED : public skel::LED {
   const int PIN_GREEN = 28;
   const int PIN_BLUE =  29;
 
-public:
-  LED(const dzn::locator& loc);
   void iLed_setGreen();
   void iLed_setYellow();
   void iLed_setRed();
   void iLed_turnOff();
+  
+public:
+  LED(const dzn::locator& loc);
 };
 
 #endif //LED_HH
--- Alarm_System_Full/src/main.cc
+++ Alarm_System_Full/src/main.cc
@@ -8,8 +8,6 @@
 #include <dzn/pump.hh>
 #include "AlarmSystem.hh"
 
-std::string password = "wachtwoord";
-
 std::function<void()> timeout;
 
 void sigalrm_handler(int signal) {
@@ -23,17 +21,13 @@ main(int argc, char* argv[])
   dzn::pump pump;
   dzn::runtime rt;
 
-  AlarmSystem as(loc.set(rt).set(pump));
+  loc.set(rt).set(pump);
+
+  AlarmSystem as(loc);
   as.dzn_meta.name = "AlarmSystem";
 
-  as.iPWManager.in.verifyPassword = [&](std::string pw){ return pw.compare(password) == 0; };
-
-  bool sensorPolling = false;
-  as.iSensor.in.turnOn = [&](){ sensorPolling = true; };
-  as.iSensor.in.turnOff = [&](){ sensorPolling = false; };
-
-  as.iTimer.in.start = [](int milliseconds){ alarm(milliseconds/1000); };  // ITimer is in milliseconds, but alarm is in seconds
-  as.iTimer.in.cancel = []{ alarm(0); };
+  as.iTimer.in.start = [] (int ms) { alarm(ms/1000); }; //ITimer contract is in milliseconds, but alarm is in seconds
+  as.iTimer.in.cancel = [] () { alarm(0); };
   timeout = as.iTimer.out.timeout;
   signal(SIGALRM, sigalrm_handler);
 
@@ -41,11 +35,6 @@ main(int argc, char* argv[])
 
   std::string input;
   while(std::cin >> input) {
-    if(input.compare("s") == 0 ) {
-      if(sensorPolling) as.iSensor.out.triggered();
-    }
-    else {
-      as.iController.in.passwordEntered(input);
-    }
+    as.iController.in.passwordEntered(input);
   }
 }
