9711cf61380437ea87c2d632884a7195123ac8c1
Jvaneerd <J.vaneerd@student.fontys.nl>
2017-06-06 09:16:28 +0200

    test

---
+++ Living_Colours_RPi/lib/pump.d
@@ -0,0 +1,3 @@
+lib/pump.d lib/pump.o: lib/pump.cc src/external_types.hh \
+ lib/dzn/locator.hh lib/dzn/pump.hh lib/dzn/coroutine.hh \
+ lib/dzn/context.hh
---
+++ Living_Colours_RPi/lib/runtime.d
@@ -0,0 +1,3 @@
+lib/runtime.d lib/runtime.o: lib/runtime.cc src/external_types.hh \
+ lib/dzn/runtime.hh lib/dzn/meta.hh lib/dzn/locator.hh lib/dzn/pump.hh \
+ lib/dzn/coroutine.hh lib/dzn/context.hh
